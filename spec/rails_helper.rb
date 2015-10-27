384056ab889453cf09342e8bd5847a3fa76cd86e
Maggie Epps <maggieepps@laborvoices.com>
2015-10-27 16:31:15 -0400

    Started test laborvoices/smartline#114

---
+++ spec/rails_helper.rb
@@ -0,0 +1,15 @@
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+# Prevent database truncation if the environment is production
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'spec_helper'
+require 'rspec/rails'
+
+# If you are not using ActiveRecord, you can remove this line.
+ActiveRecord::Migration.maintain_test_schema!
+
+RSpec.configure do |config|
+  # Use factory girl
+  config.include FactoryGirl::Syntax::Methods
+  config.infer_spec_type_from_file_location!
+end
