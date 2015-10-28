41042cc06f7d0038d486525ae3bf7d9badc8aa55
Maggie Epps <maggieepps@laborvoices.com>
2015-10-28 09:29:20 -0400

    Tested group for properties laborvoices/smartline#114

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -3,5 +3,7 @@ require 'rails_helper'
 RSpec.describe Group, type: :model do
   it 'has properties and relationships' do
     should respond_to :scores
+    should respond_to :name
+    should respond_to :conversations_count
   end
 end
