f015d2204154b895bf9f61d155ba71100df8cccc
Maggie Epps <maggieepps@laborvoices.com>
2015-10-29 11:22:38 -0400

    Correcty copy paste error

--- spec/routing/category_routing_spec.rb
+++ spec/routing/category_routing_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Group routing", :type => :routing do
+RSpec.describe "Category routing", :type => :routing do
   let(:category){FactoryGirl.create :category}
   it {is_expected.to route(:get, "/categories").to(:format => :json, :controller => 'categories', :action => 'index')}
   it {is_expected.to route(:get, "/categories/#{category.id}").to(:format => :json, :controller => 'categories', :action => 'show', :id => category.id)}
