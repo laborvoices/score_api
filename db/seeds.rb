233ec18dc18ada8d78e31639c78ced645fa78a24
Maggie Epps <maggieepps@laborvoices.com>
2015-10-29 13:03:30 -0400

    Started seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+  categories = Category.create([{ name: 'Worker Recommendation' }, { name: 'Wages'}, {name: 'Child Labor'}, {name: 'Fire Safety'}, {name: 'Child Labor'}])
+
+  groups = Group.create([
+    {
+      name: 'Factory',
+      conversations_count: 14,
+      users_count: 10,
+      total_workers: 30
+      },
+
+    ])
