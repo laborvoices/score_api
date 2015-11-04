0283c4a3a712956cd8319105830bbce2881f8c50
Maggie Epps <maggieepps@laborvoices.com>
2015-11-04 17:38:00 -0500

    Testing fix for authenticity issue

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,7 @@
 class GroupsController < ApplicationController
   respond_to :json
   before_action :set_categories
+  skip_before_filter :verify_authenticity_token
 
   def index
     @groups = Group.all
